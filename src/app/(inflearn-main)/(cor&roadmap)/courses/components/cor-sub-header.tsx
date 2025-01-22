071b206e148bc7424c6cacdab6a9f2eaa4f9f8d6
HongJinSuk <realtone98@penta.co.kr>
2025-01-22 16:18:29 +0900

    cor-sub-header right left click scroll

--- src/app/(inflearn-main)/(cor&roadmap)/courses/components/cor-sub-header.tsx
+++ src/app/(inflearn-main)/(cor&roadmap)/courses/components/cor-sub-header.tsx
@@ -7,6 +7,8 @@ import {
   BookUser,
   Brain,
   BriefcaseBusiness,
+  ChevronLeft,
+  ChevronRight,
   Cpu,
   Gamepad2,
   Laptop,
@@ -34,52 +36,69 @@ const subHeaderMenus: SubHeaderMenusData[] = [
 ];
 export default function CorSubHeader() {
   const scrollRef = useRef<HTMLDivElement>(null);
-  const [isHover, setIsHover] = useState(false);
+  const [chevronDir, setChevronDir] = useState<'left' | 'right'>('right');
 
   useEffect(() => {
-    const handleWheel = (e: WheelEvent) => {
-      if (!scrollRef.current || !isHover) return;
+    const handleScrollEvent = () => {
+      if (scrollRef.current) {
+        const currentScroll = scrollRef.current.scrollLeft;
+        const maxScroll =
+          scrollRef.current.scrollWidth - scrollRef.current.clientWidth;
 
-      e.preventDefault();
-      scrollRef.current.scrollLeft += e.deltaY;
-    };
-
-    const refElement = scrollRef.current;
-
-    if (refElement) {
-      refElement.addEventListener('wheel', handleWheel, { passive: false });
-    }
-
-    return () => {
-      if (refElement) {
-        refElement.removeEventListener('wheel', handleWheel);
+        if (currentScroll <= 0) {
+          setChevronDir('right');
+        } else if (currentScroll >= maxScroll) {
+          setChevronDir('left');
+        }
       }
     };
-  }, [isHover]);
 
-  // const handleScroll = (dir: 'left' | 'right') => {
-  //   if (scrollRef.current) {
-  //     const scrollAmount = 1000; // 스크롤 거리
-  //     scrollRef.current.scrollBy({
-  //       left: dir === 'left' ? -scrollAmount : scrollAmount,
-  //       behavior: 'smooth',
-  //     });
-  //   }
-  // };
+    scrollRef.current?.addEventListener('scroll', handleScrollEvent);
+
+    return () => {
+      scrollRef.current?.removeEventListener('scroll', handleScrollEvent);
+    };
+  }, []);
+
+  const handleScroll = (dir: 'left' | 'right') => {
+    if (scrollRef.current) {
+      const scrollAmount = 1000; // 스크롤 거리
+      scrollRef.current.scrollBy({
+        left: dir === 'left' ? -scrollAmount : scrollAmount,
+        behavior: 'smooth',
+      });
+    }
+  };
 
   return (
     <div className="flex items-center h-20 w-full max-w-[1440px] min-w-[500px] mx-auto px-4 relative bg-white">
-      {/* <div className="absolute left-4 top-0 h-full w-20 pointer-events-none bg-gradient-to-r from-red-400 to-transparent z-10" /> */}
+      <ChevronLeft
+        onClick={() => {
+          handleScroll('left');
+        }}
+        className={`${
+          chevronDir === 'right' ? 'hidden' : ''
+        } absolute left-8 z-10 cursor-pointer bg-white border size-8 rounded-full p-1`}
+        strokeWidth={1}
+      />
       <div
         className="flex items-center w-[97vw] px-4 overflow-x-auto scroll-container"
-        onMouseEnter={() => setIsHover(true)} // 마우스 오버 시 활성화
-        onMouseLeave={() => setIsHover(false)} // 마우스 나가면 비활성화
+        // onMouseEnter={() => setIsHover(true)} // 마우스 오버 시 활성화
+        // onMouseLeave={() => setIsHover(false)} // 마우스 나가면 비활성화
         ref={scrollRef}
+        style={{
+          maskImage:
+            chevronDir === 'left'
+              ? 'linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1) 10%, rgba(0, 0, 0, 1) 100%, rgba(0, 0, 0, 0))'
+              : 'linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 1) 80%, rgba(0, 0, 0, 0))',
+          WebkitMaskImage:
+            chevronDir === 'left'
+              ? 'linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1) 10%, rgba(0, 0, 0, 1) 100%, rgba(0, 0, 0, 0))'
+              : 'linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 1) 80%, rgba(0, 0, 0, 0))',
+          maskRepeat: 'no-repeat',
+          WebkitMaskRepeat: 'no-repeat',
+        }}
       >
-        {/* <ChevronLeftCircle
-          onClick={() => handleScroll('left')}
-          className="absolute left-8 z-50 cursor-pointer"
-        /> */}
         <div className="flex flex-col items-center justify-center space-y-2">
           <Search strokeWidth={2} />
           <span className="text-xs font-medium">검색</span>
@@ -98,11 +117,39 @@ export default function CorSubHeader() {
             </div>
           ))}
         </div>
-        {/* <ChevronRightCircle
-          onClick={() => handleScroll('right')}
-          className="absolute right-8 z-10 cursor-pointer"
-        /> */}
       </div>
+      <ChevronRight
+        onClick={() => {
+          handleScroll('right');
+        }}
+        className={`${
+          chevronDir === 'left' ? 'hidden' : ''
+        } absolute right-8 z-10 cursor-pointer bg-white border size-8 rounded-full p-1`}
+        strokeWidth={1}
+      />
     </div>
   );
 }
+
+// 마우스 호버하고 스크롤시 움직이게 하는 기능
+// const [isHover, setIsHover] = useState(false);
+// useEffect(() => {
+//   const handleWheel = (e: WheelEvent) => {
+//     if (!scrollRef.current || !isHover) return;
+
+//     e.preventDefault();
+//     scrollRef.current.scrollLeft += e.deltaY;
+//   };
+
+//   const refElement = scrollRef.current;
+
+//   if (refElement) {
+//     refElement.addEventListener('wheel', handleWheel, { passive: false });
+//   }
+
+//   return () => {
+//     if (refElement) {
+//       refElement.removeEventListener('wheel', handleWheel);
+//     }
+//   };
+// }, [isHover]);
